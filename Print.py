923a7a7e09cd69196843d310f169919eb3b5c014
Harsh vardhan Yadav <princeyadav49214@gmail.com>
2025-08-03 19:13:20 +0530

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,28 @@
+#Print is a predefine function which use to print massage or variable.
+print("Hello, Prince.")
+
+#Massage write in double quotes but variable write without quotes.
+#Variable is container which hold the value.
+name="Prince Yadav"           #String also store in duoble quotes.
+print(name)
+print("Hello ",name)
+#comma is use to spriate the the massage and variable.
+
+#Variable data type dynamic type cast
+a=10
+b=20
+print("Sum = ",a+b)
+
+#check data type of variable
+x=20
+y="Harika"
+print(type(x))
+print(type(y))
+
+# add number to a variable 
+x = 10
+print(x+1)
+
+# assign multiple variables
+a,b,c = 1,2,3
+print("A",a,"B",b,"C",c,sep="=")
